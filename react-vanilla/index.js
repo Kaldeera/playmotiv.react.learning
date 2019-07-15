329ac7629eda69eee86f74996ef4c2bcc9c750a4
albertomoral <alberto.moral@poeticsoft.com>
2019-07-15 11:01:58 +0200

    Change name functions to functions-props

--- react-vanilla/index.js
+++ react-vanilla/index.js
@@ -1,36 +1,36 @@
-const createElement = React.createElement
+const el = React.createElement
 
 function render() {
   const value = Math.floor(Math.random() * 10000)
 
-  const App = createElement(
+  const App = el(
     'div',
     {
       className: 'App'
     },
     [
-      createElement('h1', {}, `React Functions [ ${value} ]`),
-      createElement(
+      el('h1', {}, `React Functions [ ${value} ]`),
+      el(
         'div',
         {
           className: 'Level 1'
         },
-        createElement('div', {}, [
+        el('div', {}, [
           'LEVEL 1',
-          createElement(
+          el(
             'div',
             {
               className: 'Level 2'
             },
             'LEVEL 2',
-            createElement(
+            el(
               'div',
               {
                 className: 'Level 3'
               },
               [
                 'LEVEL 3',
-                createElement(
+                el(
                   'button',
                   {
                     onClick: render
