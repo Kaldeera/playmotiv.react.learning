1fb987468936cf4874972cf17cc9a3bda22c4246
Dani Servian <daniserviandev@gmail.com>
2019-07-16 11:29:35 +0200

    Fix react-vanilla 'keys' error

--- react-vanilla/index.js
+++ react-vanilla/index.js
@@ -7,7 +7,7 @@ function render() {
   const App = el(
     'div',
     {
-      className: 'App Otra'
+      className: 'App Otra',
     },
     [
       el('h1', { key: 0 }, `React Functions [ ${value} ]`),
@@ -15,27 +15,30 @@ function render() {
         'div',
         {
           className: 'Level 1',
-           key: 1
+          key: 1
         },
         el('div', {}, [
-          'LEVEL 1',
+          el('span', { key: 2 }, 'LEVEL 1'),
           el(
             'div',
             {
-              className: 'Level 2'
+              className: 'Level 2',
+              key: 3
             },
-            'LEVEL 2',
+            el('span', { key: 4 }, 'LEVEL 2'),
             el(
               'div',
               {
-                className: 'Level 3'
+                className: 'Level 3',
+                key: 5
               },
               [
-                'LEVEL 3',
+                el('span', { key: 6 }, 'LEVEL 3'),
                 el(
                   'button',
                   {
-                    onClick: render
+                    onClick: render,
+                    key: 7
                   },
                   'CHANGE'
                 )
