8f9eb9383a0446aff2d4cbd4a2f165aacf187445
albertomoral <alberto.moral@poeticsoft.com>
2019-11-28 13:24:24 +0100

    react-scss

---
+++ react-scss/src/components/app.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../scss/main.scss'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <div className="ToolBar"></div>
+      <div className="SideBar"></div>
+      <div className="Editor">
+        <div className="Content">
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+          <div className="Box"></div>
+        </div>
+        <div className="Footer"></div>
+      </div>
+    </div>
+  )
+}
+
+export default App
